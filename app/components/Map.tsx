19e93d3ba4ac5675dbde7c906bbda3fd13d9874c
Vangelis Sigalas <yourtube12321@gmail.com>
2024-03-07 15:36:47 +0200

    if(typeof window !== 'undefined') again

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,16 +5,18 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 
 const Map = () => {
-  return (
-    <div className="map-wrap bg-[#333333] overflow-hidden z-[5] rounded-lg border-2 border-[#333333] max-w-[1000px] self-center w-full">
-      <MapContainer center={[38.00829, 23.67664]} zoom={13}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[38.00829, 23.67664]}>
-          <Popup>Ξυλοεφαρμογές</Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  if (typeof window !== "undefined") {
+    return (
+      <div className="map-wrap bg-[#333333] overflow-hidden z-[5] rounded-lg border-2 border-[#333333] max-w-[1000px] self-center w-full">
+        <MapContainer center={[38.00829, 23.67664]} zoom={13}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={[38.00829, 23.67664]}>
+            <Popup>Ξυλοεφαρμογές</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    );
+  }
 };
 
 export default Map;
