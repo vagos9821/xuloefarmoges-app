00647ddb7792542adea82050d6e8b0fcf9a5ef81
Vangelis Sigalas <yourtube12321@gmail.com>
2024-03-07 17:18:26 +0200

    fonts

--- app/components/Hero1.tsx
+++ app/components/Hero1.tsx
@@ -1,10 +1,19 @@
+import { Playfair_Display } from "next/font/google";
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
+const playFair = Playfair_Display({
+  subsets: ["latin"],
+});
+
 const Hero1 = () => {
   return (
-    <div className=" bg-[url('/slide-2.jpg')] bg-cover w-full h-[600px] flex flex-col justify-center align-middle text-center text-white gap-10 p-6">
-      <h1 className="text-3xl font-extrabold">
+    <div className=" bg-[url('/slide-2.jpg')] bg-cover w-full h-[600px] flex flex-col justify-center align-middle text-center text-white gap-10 p-6 ">
+      <Head>{""}</Head>
+      <h1
+        className={`${playFair.className} text-4xl font-extrabold tracking-widest`}
+      >
         Κάνουμε Τα Όνειρα Σας Πραγματικότητα
       </h1>
       <p>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MenuButton from "./MenuButton";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="h-[80px] w-full bg-[#333333] flex justify-around fixed z-10 text-white">
       <div className="flex">
-        <img src="/logo-white.png" alt="" className="h-[60px] self-center" />
+        <Link href="/" className="self-center">
+          <img src="/logo1-white.png" alt="" className="h-[60px] " />
+        </Link>
       </div>
       <MenuButton />
     </div>
